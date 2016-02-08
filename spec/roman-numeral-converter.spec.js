ceac18dcc5def9d7432f6f8aded3bdb8392c1612
Sergey Bolshov <bolszow@gmail.com>
2016-02-08 23:40:58 +0100

    convertToRoman function API test and tests for 7 base roman symbols

--- spec/roman-numeral-converter.spec.js
+++ spec/roman-numeral-converter.spec.js
@@ -5,32 +5,59 @@
 
   describe('Roman numeral converter', function() {
 
-    describe('Convert to roman', function() {
+    describe('Convert to roman function test', function() {
 
-      it('convertToRoman must be a function', function(){
-        expect(typeof converter.convertToRoman).toBe('function');
+      describe('API test', function() {
+        it('convertToRoman must be a function', function() {
+          expect(typeof converter.convertToRoman).toBe('function');
+        });
+
+        it('throws when the argument is not a number', function() {
+          expect(function() {
+            converter.convertToRoman();
+          }).toThrowError(TypeError);
+          expect(function() {
+            converter.convertToRoman(null);
+          }).toThrowError(TypeError);
+          expect(function() {
+            converter.convertToRoman('');
+          }).toThrowError(TypeError);
+          expect(function() {
+            converter.convertToRoman([]);
+          }).toThrowError(TypeError);
+        });
       });
 
-      it('returns I', function(){
-        expect(converter.convertToRoman(1)).toBe('I');
+      describe('Test base 7 symbols', function() {
+        it('returns I', function() {
+          expect(converter.convertToRoman(1)).toBe('I');
+        });
+
+        it('returns V', function() {
+          expect(converter.convertToRoman(5)).toBe('V');
+        });
+
+        it('returns X', function() {
+          expect(converter.convertToRoman(10)).toBe('X');
+        });
+
+        it('returns L', function() {
+          expect(converter.convertToRoman(50)).toBe('L');
+        });
+
+        it('returns C', function() {
+          expect(converter.convertToRoman(100)).toBe('C');
+        });
+
+        it('returns D', function() {
+          expect(converter.convertToRoman(500)).toBe('D');
+        });
+
+        it('returns M', function() {
+          expect(converter.convertToRoman(1000)).toBe('M');
+        });
       });
 
-      it('returns II', function(){
-        expect(converter.convertToRoman(2)).toBe('II');
-      });
-
-      it('returns IV', function(){
-        expect(converter.convertToRoman(4)).toBe('IV');
-      });
-
-      it('returns V', function(){
-        expect(converter.convertToRoman(5)).toBe('V');
-      });
-
-      it('returns X', function(){
-        expect(converter.convertToRoman(10)).toBe('X');
-      });
     });
-
   });
 }(window));
--- src/roman-numeral-converter.js
+++ src/roman-numeral-converter.js
@@ -3,13 +3,18 @@
 
   var values = {
     '1': 'I',
-    '2': 'II',
-    '4': 'IV',
     '5': 'V',
-    '10': 'X'
+    '10': 'X',
+    '50': 'L',
+    '100': 'C',
+    '500': 'D',
+    '1000': 'M'
   };
 
   var ConvertToRoman = function(value) {
+    if (typeof value !== 'number' || isNaN(value) || !isFinite(value)) {
+      throw new TypeError('Value must be a number');
+    }
     return values[value];
   };
 
